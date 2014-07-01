9bf5b99dfe90100914fd245ec3acc397d060a014
xxxkurosukexxx <info@kurosuke.org>
2014-07-01 14:52:10 +0900

    とりあえず現状のコードをコミット
    Composerにはまだ頼ってない。
    config.incは公開用に値を消してある。

---
+++ src/index.js
@@ -0,0 +1,126 @@
+function calcLimit(obj) {
+    var limit = 140 - obj.val().length, dispObj = $("#limit");
+    dispObj.text(limit);
+    if (limit < 0) {
+        dispObj.addClass("over");
+    } else {
+        dispObj.removeClass("over");
+    }
+}
+
+function resize() {
+    window.resizeTo(700, 185);
+}
+
+window.onload = resize;
+window.onresize = resize;
+
+$(function() {
+    // 本文
+    var msg = $("#msg");
+    msg.focus();
+
+    function init(){
+        $.each(twitterAccount, function(){
+            $("#idselect").append($("<option />").val(this).text(this));
+        });
+        $.each(facesConfig, function() {
+            $("#faceSelect").append($("<option />").val(this).text(this));
+        });
+        $("#twitterid").val($("#idselect").val());
+        chgColor($("#idselect").val());
+    }
+    init();
+
+    // 本文入力時のEnterキーの処理
+    $(document).on("keypress", "#msg", function(e) {
+        if (e.keyCode == 13) {// Enterが押された
+            if (e.shiftKey) {// Shiftキーも押された
+                $.noop();
+            } else if (msg.val().replace(/\s/g, "").length > 0) {
+                e.preventDefault();
+                $.post(window.location.toString(), $("#form").serializeArray(), function(res) {
+                    if (res.status == "OK") {
+                        $(".msg").val("");
+                        calcLimit(msg);
+                        $("#face").text("( 'ω') ok.");
+                    } else {
+                        alert(res.message);
+                        $("#face").text("( 'ω') error...");
+                    }
+                    return false;
+                });
+            }
+        }
+    });
+
+    // 本文入力時の文字数計算処理
+    msg.bind("input", function() {
+        calcLimit(msg);
+    });
+
+    // clearボタン
+    // 本文、リプIDをクリアする。
+    $("#clearBtn").click(function() {
+        $(".msg").val('');
+    });
+
+    // repボタン
+    // 最新のリプライを取得し、リプテキストをalert表示、本文に@ほげほげ,repIdにふがふがをセットする。
+    $("#repGetBtn").click(function() {
+        $.post(window.location.toString(), {
+            "mode" : "repGet",
+            "id"   : $("#idselect").val()
+        }, function(res) {
+            if (res.status == "OK") {
+                var msg = res.message[0];
+                alert("\"@" + msg.user.screen_name + " " + msg.user.name + "\"\n" + msg.text);
+                $("#repId").val(msg.id_str);
+                $("#msg").val("@" + msg.user.screen_name + " " + $("#msg").val());
+            } else {
+                alert("( 'ω') error...");
+            }
+            return false;
+        });
+    });
+
+    // delボタン
+    // 最新のツイートを問答無用で削除する。
+    $("#delBtn").click(function() {
+        $.post(window.location.toString(), {
+            "mode" : "del",
+            "id"   : $("#idselect").val()
+        }, function(res) {
+            if (res.status == "OK") {
+                alert("( 'ω') ok.");
+            } else {
+                alert("( 'ω') error...");
+            }
+            return false;
+        });
+    });
+
+    // 顔文字
+    // 選んだものを本文の先頭に突っ込む♂
+    $("#faceSelect").change(function() {
+        msg.focus();
+        msg.val($(this).val() + msg.val());
+        calcLimit(msg);
+        $(this).val("");
+    });
+
+    // ID切替
+    // 切り替えた時に色を変える（誤爆防止）
+    $("#idselect").change(function() {
+        msg.focus();
+        chgColor($(this).val());
+        $("#twitterid").val($(this).val());
+    });
+
+    function chgColor(id) {
+        $("*").css("color", colorConfig[id]);
+        $(".msg:focus").css("outline-color", colorConfig[id]);
+    }
+    
+})
+
---
+++ src/index.php
@@ -0,0 +1,97 @@
+<?php
+    require_once ('config.inc');
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        require_once ('Services/Twitter.php');
+        require_once ('HTTP/OAuth/Consumer.php');
+
+        $returnArr = array();
+
+        try {
+            $twitter = new Services_Twitter();
+            $id = $_POST["id"];
+
+            // *----- oAuth -----* //
+            $oauth = new HTTP_OAuth_Consumer($consumer_key, $consumer_secret, $auth_token[$id], $token_secret[$id]);
+            $twitter -> setOption("use_ssl", true);
+            $twitter -> setOAuth($oauth);
+            // *----- oAuth -----* //
+
+            // *----- main -----* //
+            switch($_POST["mode"]) {
+                case "post" :
+                    $postParams = array("status" => $_POST["msg"]);
+                    if(!empty($_POST["repId"])) {
+                        $postParams["in_reply_to_status_id"] = $_POST["repId"];
+                    }
+                    $msg = $twitter -> statuses -> update($postParams);
+                    break;
+                case "del" :
+                    $statuses = $twitter -> statuses -> user_timeline();
+                    $msg = $twitter -> statuses -> destroy($statuses[0] -> id_str);
+                    break;
+                case "repGet" :
+                    $msg = $twitter -> statuses -> mentions_timeline(array("count" => 1));
+                    break;
+            }
+            // *----- main -----* //
+
+            $returnArr = array(
+                "status" => "OK",
+                "message" => $msg
+            );
+        } catch(Services_Twitter_Exception $e) {
+            error_log(var_export($e -> getMessage(), true));
+            $returnArr = array(
+                "status" => "NG",
+                "message" => $e -> getMessage()
+            );
+        }
+
+        header('Content-type: application/json');
+        echo json_encode($returnArr);
+    } else {
+        header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <script src="jquery-2.0.3.min.js"></script>
+        <script src="index.js"></script>
+        <title>tweet-at-work</title>
+        <link href="index.css" rel="stylesheet" type="text/css" />
+        <script>
+            var twitterAccount = <?php echo json_encode($twitterAccount); ?>;
+            var colorConfig    = <?php echo json_encode($colorConfig); ?>;
+            var facesConfig    = <?php echo json_encode($facesConfig); ?>;
+        </script>
+    </head>
+    <body>
+        <span id="face">( 'ω') ?</span> <span id="limit">140</span>
+        <span id="btns">
+            <button id="repGetBtn" title="rep">
+                rep
+            </button>
+            <button id="clearBtn" title="clear">
+                clear
+            </button>
+            <button id="delBtn" title="undo">
+                undo
+            </button>
+            <select id="faceSelect">
+                <option value="">☻</option>
+            </select>
+            <select id="idselect">
+            </select>
+        </span>
+        <form method="post" id="form">
+            <textarea class="msg" id="msg" name="msg" rows="3" cols="80"></textarea><br />
+            <label>Reply to : </label><input class="msg" id="repId" type="text" name="repId" readonly="true" />
+            <input type="hidden" name="mode" value="post" />
+            <input type="hidden" name="id" id="twitterid" value="" />
+        </form>
+    </body>
+</html>
+<?php
+    } //GETここまで
+?>
